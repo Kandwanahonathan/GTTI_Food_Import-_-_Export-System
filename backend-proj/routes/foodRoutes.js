926b02c2b64683d58796dd981edb8f4732747eac
Akandwanaho Nathan <nathankandwanaho3@gmail.com>
2026-05-19 02:01:43 -0700

    updates

--- backend-proj/routes/foodRoutes.js
+++ backend-proj/routes/foodRoutes.js
@@ -16,4 +16,42 @@ router.post('/addFood',async(req,res)=>{
         
     }
 })
+
+// select food in the database
+
+router.get('/foodList', async(req,res)=>{
+    try {
+        const foodList= await food.find()
+        return res.status(200).json({success:true ,list:foodList})
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({Error:err})
+    }
+})
+
+//update the data in database
+
+router.put('/updateFood/:_id', async(req,res) =>{
+    try {
+        const {_id}=req.params
+         const {Food_Id,Food_Name,Food_OwnerName} =req.body
+
+         const updateFood=await food.findByIdAndUpdate(_id,{Food_Id,Food_Name,Food_OwnerName},{returnDocument:'after'})
+         return res.status(200).json({message:"food updated successfully",update:updateFood})
+    } catch (err) {
+         console.log(err);
+        return res.status(500).json({Error:err})
+    }
+})
+// delete data in the database
+router.delete('/deleteFood/:_id', async(req,res)=>{
+    try {
+        const {_id} =req.params
+        const deleteFood=await food.findByIdAndDelete(_id)
+        return res.status(200).json({message:"food Deleted succcessfully",delete:deleteFood})
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({Error:err})
+    }
+})
 module.exports=router
